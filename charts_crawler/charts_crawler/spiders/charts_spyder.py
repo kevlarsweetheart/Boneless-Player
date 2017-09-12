25e98266e4f1f6ecfaea91093c4d49976a9fc33a
Maria Efremova <maria@dell7537-masha>
2017-09-12 23:09:39 +0300

    Downloading Billboad charts

--- charts_crawler/charts_crawler/spiders/charts_spyder.py
+++ charts_crawler/charts_crawler/spiders/charts_spyder.py
@@ -1,21 +1,56 @@
 import scrapy
 from datetime import date
+import re
 
 class BillBoardSpider(scrapy.Spider):
-    name = 'billboard'
+    name = 'Billboard'
     start_urls = ['http://www.billboard.com/archive/charts/']
     allowed_domains = ['billboard.com']
+
+    # From the chart page extract information about positions and store it in format
+    # {'artist': <artist_name>, 'song_name': <song_name>, 'cur_pos': <current chart position>,
+    #  'last_week_pos': <last week chart position>, 'delta': <last week and current positions delta>}
+    def parse_chart(self, response):
+        request_url = response.url.split("/")
+        positions = response.xpath('//article[re:test(@class, "chart-row chart-row--\d+ js-chart-row")]')
+        for row in positions:
+            pos_dict = {}
+            artist = row.xpath('.//a[contains(@class, "chart-row__artist")]/text()').extract_first()
+            if not artist:
+                artist = row.xpath('.//span[contains(@class, "chart-row__artist")]/text()').extract_first()
+            artist = re.sub('\n', '', artist)
+            pos_dict['Artist name'] = artist
+            title = row.xpath('.//h2[contains(@class, "chart-row__song")]/text()').extract_first()
+            cur_pos = row.xpath('.//span[contains(@class, "chart-row__current-week")]/text()').extract_first()
+            pos_dict['Current position'] = cur_pos
+            last_week = row.xpath('.//span[contains(@class, "chart-row__last-week")]/text()').extract_first()
+            pos_found = re.search(' \d+', last_week)
+            if pos_found:
+                last_week_pos = last_week[pos_found.start():pos_found.end()]
+                pos_dict['Last week position'] = last_week_pos
+                delta = int(last_week_pos) - int(cur_pos)
+            else:
+                delta = 0
+                pos_dict['Last week position'] = 'New'
+            pos_dict['Chart gain'] = delta
+            if 'hot-100' in request_url:
+                pos_dict['Song title'] = title
+            elif 'billboard-200' in request_url:
+                pos_dict['Album title'] = title
+            yield pos_dict
 
 
     def parse_date(self, response):
         table = response.xpath('//table[contains(@class, "views-table")]')
         a_list = table.xpath('.//a/@href').extract()
         a_list = [a for a in a_list if a.split('/')[1] == 'charts']
-        print(a_list)
+        for a in a_list:
+            new_page = "http://www.billboard.com" + a
+            yield scrapy.Request(new_page, callback=self.parse_chart)
 
 
     def parse_year(self, response):
-        wanted_charts = {'The Hot 100': '/hot-100', 'Billboard 200': '/billboard-200', 'Radio Songs': '/radio-songs', 'Artist 100': '/artist-100'}
+        wanted_charts = {'The Hot 100': '/hot-100', 'Billboard 200': '/billboard-200', 'Artist 100': '/artist-100'}
         charts = response.xpath('//span[contains(@class, "field-content")]/a/text()').extract()
         charts = [c for c in charts if c in wanted_charts.keys()]
         for c in charts:
